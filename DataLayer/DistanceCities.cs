eee93e7f2978121a6e8ca60fd6cec3c9a22b9011
Fatih Aktürk <fatihakturk1453@gmail.com>
2023-08-20 19:37:29 +0300

    Statik liste eklendi ve denemesi yapıldı

---
+++ DataLayer/DistanceCities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class DistanceCities
+    {
+        public static int[,,] kmList = new int[,,]
+        {
+            {{44,6,652}},
+            {{44,17,1304}},
+            {{44,17,1304}},
+            {{44,34,1103}},
+            {{44,35,1190}},
+            {{6,17,665}},
+            {{6,34,453}},
+            {{6,35,584}},
+            {{17,34,316}},
+            {{17,35,327}},
+            {{34,35,566}}
+        };
+
+        public static IDictionary<(int, int), int> cities = new System.Collections.Generic.Dictionary<(int, int), int>()
+        {
+            {(44,6),652},
+            {(44,17),1304},
+            {(44,34),1103},
+            {(44,35),1190},
+            {(6,17),665},
+            {(6,34),453},
+            {(6,35),584},
+            {(17,34),316},
+            {(17,35),327},
+            {(34,35),566}
+        };
+
+        public static IDictionary<int, string> cityNames = new Dictionary<int, string>()
+        {
+             {44, "Malatya"},
+             {6, "Ankara"},
+             {17, "Çanakkale"},
+             {34, "İstanbul"},
+             {35, "İzmir"}
+        };
+
+    }
+}
--- TasimacilikAppConsole/Program.cs
+++ TasimacilikAppConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Abstract;
+﻿using DataLayer;
+using EntityLayer.Abstract;
 using EntityLayer.Concrete.Cargos;
 using EntityLayer.Concrete.Orders;
 using EntityLayer.Concrete.Persons;
@@ -13,16 +14,21 @@ cargo.ID = 1;
 cargo.Name="Yeşil Mercimek";
 IPerson person = new Driver();
 person.ID = 1;
-person.Name = "Bilge Tufan ÖZDEMİR";
+person.Name = "Fatih Aktürk";
 Operation opr = new Operation();
 
 opr.Vehicle = vehicle;
 opr.Order = order;
 opr.Cargo = cargo;
 opr.Person = person;
-
+/*
 opr.loadOnWay(); //YÜK YOLA ÇIKTI
 Thread.Sleep(5000); //5 SANİYE ARA
 opr.loadIsArrived(); //YÜK YERİNE ULAŞTI
 opr.Order.applyPayment(); //ÖDEME TAMAMLANDI
+*/
 
+
+
+//2 Şehir arası km hesaplama denemeleri
+Console.WriteLine(DistanceCities.cities[(44,17)]);
